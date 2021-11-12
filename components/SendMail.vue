2bf7e6ad72d4bed8efe6d0d496e7119e5db91dd7
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-11-12 18:12:07 +0600

    successful form submission and mail sent

---
+++ components/SendMail.vue
@@ -0,0 +1,43 @@
+<template>
+  <form id="form" ref="form" v-on:submit.prevent="submit">
+    <input placeholder="Name" v-model="name" type="text" />
+    <input placeholder="Email" v-model="email" type="email" />
+    <input placeholder="Subject" v-model="subject" type="text" />
+    <textarea placeholder="Message" v-model="message">Message</textarea>
+    <button type="submit">Send Me An Email</button>
+
+    <p>{{ message }}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    };
+  },
+
+  methods: {
+    submit() {
+      fetch("http://localhost:3001/sendmail", {
+        method: "POST",
+
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: this.name,
+          email: this.email,
+          subject: this.subject,
+          message: this.message,
+        }),
+      });
+    },
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <SendMail />
+</template>
+
+<script>
+export default {};
+</script>
